ce8bf29a2237a9c08ca2aa17d47af0b5eab4ce0c
Yoon Kim <yoonkim@berkeley.edu>
2016-11-12 07:34:09 -0800

    information collecting

--- app/views.py
+++ app/views.py
@@ -1,21 +1,30 @@
-from flask import render_template
+from flask import render_template, Flask, jsonify
 from app import app
+from functions import *
+import requests
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+@app.route('/search/api/v1.0/flight-info', methods=['GET'])
+def get_flight_info():
+    flight = requests.get('https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=R26ZAzuBsJnmMFFX2RVh0qEK2PpDLgPx&origin=BOS&destination=LON&departure_date=2016-11-25&nonstop=true')
+    json_object = flight.json()
+    return jsonify(json_object['results'])
+
+# @app.route('/')
+# @app.route('/index')
+
+# def index():
+#     user = {'nickname': 'Miguel'}  # fake user
+#     posts = [  # fake array of posts
+#         {
+#             'author': {'nickname': 'John'},
+#             'body': 'Beautiful day in Portland!'
+#         },
+#         {
+#             'author': {'nickname': 'Susan'},
+#             'body': 'The Avengers movie was so cool!'
+#         }
+#     ]
+#     return render_template("index.html",
+#                            title='Home',
+#                            user=user,
+#                            posts=posts)
