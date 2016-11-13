6e7ef26b4ac154084fd38aca5e61415a25686386
Yoon Kim <yoonkim@berkeley.edu>
2016-11-12 17:51:31 -0800

    APIs added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, Flask, jsonify
 from app import app
-from functions import *
+from . import functions
 import requests
 
 @app.route('/')
@@ -16,15 +16,15 @@ def get_flight_info():
 
 @app.route('/search/api/v1.0/drive-info', methods=['GET'])
 def get_drive_info():
-    drive = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=75+9th+Ave+New+York,+NY&destination=MetLife+Stadium+1+MetLife+Stadium+Dr+East+Rutherford,+NJ+07073&key=AIzaSyAupEuZynix3YD4F9QTknDAdLNEEIUqX7k')
+    drive = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=Boston+Logan+International+Airport,+1+Harborside+Dr,+Boston,+MA+02128&destination=John+F.+Kennedy+International+Airport,+New+York,+NY+11430&key=AIzaSyAupEuZynix3YD4F9QTknDAdLNEEIUqX7k')
     json_object = drive.json()
-    return jsonify(json_object)
-    
+    return jsonify(json_object['routes'])
+
 @app.route('/search/api/v1.0/transit-info', methods=['GET'])
 def get_transit_info():
-    transit = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=75+9th+Ave+New+York,+NY&destination=MetLife+Stadium+1+MetLife+Stadium+Dr+East+Rutherford,+NJ+07073&mode=transit&arrival_time=1391374800&key=AIzaSyAupEuZynix3YD4F9QTknDAdLNEEIUqX7k')
+    transit = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=Boston+Logan+International+Airport,+1+Harborside+Dr,+Boston,+MA+02128&destination=John+F.+Kennedy+International+Airport,+New+York,+NY+11430&mode=transit&key=AIzaSyAupEuZynix3YD4F9QTknDAdLNEEIUqX7k')
     json_object = transit.json()
-    return jsonify(json_object)
+    return jsonify(json_object['routes'])
 
 # @app.route('/')
 # @app.route('/index')
